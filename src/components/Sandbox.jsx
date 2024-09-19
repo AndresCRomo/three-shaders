dcab4281e4eb0519a85bf5270c1826212baf37c8
AndresCRomo <andres.calderon@miaa.mx>
2024-09-19 16:03:00 -0600

    segundo commit

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -5,19 +5,22 @@ import { extend } from "@react-three/fiber";
 import vertexShader from "../shaders/vertex.glsl";
 import fragmentShader from "../shaders/fragment.glsl";
 
-const CustomMaterial = shaderMaterial({}, vertexShader, fragmentShader);
+const CustomMaterial = shaderMaterial({}, vertexShader, fragmentShader,);
 
 extend({ CustomMaterial });
 
 const Sandbox = () => {
     return (
-        <Canvas camera={{ position: [2, 2, 2], fov: 75 }}>
+        <Canvas
+        camera={{ position: [2, 2, 2], fov: 75 }}
+        style={{ background: "#000000" }}
+        >
         <Suspense fallback={null}>
             <ambientLight />
             <OrbitControls enableZoom={true} enableRotate={true} />
             <mesh>
-            <sphereGeometry args={[1, 2, 64]} />
-            <customMaterial />
+            <planeGeometry args={[2, 2]} />
+            <customMaterial wireframe={false}  />
             </mesh>
         </Suspense>
         </Canvas>
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,6 +1,10 @@
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+varying vec2 vUv;
 void main() {
-    vec3 color = vec3(1.0, 0.0, 1.0); // Color
+    vec3 color = vec3(0.3, 0.0, 1.0); // Color
     
-    gl_FragColor = vec4(color, 1.0); // Color
+    gl_FragColor = vec4(vUv.xxx, 1.0); // Color
 
 }

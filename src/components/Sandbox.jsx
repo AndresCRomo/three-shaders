50ddc9ac53d39f478e28b02ab145636662ab13b5
AndresCRomo <andres.calderon@miaa.mx>
2024-09-20 15:24:32 -0600

    tercer commit

--- src/components/Sandbox.jsx
+++ src/components/Sandbox.jsx
@@ -1,26 +1,35 @@
 import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls, shaderMaterial } from "@react-three/drei";
 import { extend } from "@react-three/fiber";
+import * as THREE from "three";
 import vertexShader from "../shaders/vertex.glsl";
 import fragmentShader from "../shaders/fragment.glsl";
+import colorfulTexture from "../images/image.jpg";
 
-const CustomMaterial = shaderMaterial({}, vertexShader, fragmentShader,);
+// Define the custom shader material
+const CustomMaterial = shaderMaterial(
+    { uTexture: null }, // Define initial uniform for texture
+    vertexShader,
+    fragmentShader
+);
 
+// Extend the shader material
 extend({ CustomMaterial });
 
 const Sandbox = () => {
+  // Load texture using useLoader
+    const texture = useLoader(THREE.TextureLoader, colorfulTexture);
+
     return (
-        <Canvas
-        camera={{ position: [2, 2, 2], fov: 75 }}
-        style={{ background: "#000000" }}
-        >
+        <Canvas style={{ background: "#666666" }}>
         <Suspense fallback={null}>
             <ambientLight />
             <OrbitControls enableZoom={true} enableRotate={true} />
             <mesh>
-            <planeGeometry args={[2, 2]} />
-            <customMaterial wireframe={false}  />
+            <planeGeometry args={[2, 2, 10, 10]} />
+            {/* Pass the texture as a uniform */}
+            <customMaterial uTexture={texture} />
             </mesh>
         </Suspense>
         </Canvas>
